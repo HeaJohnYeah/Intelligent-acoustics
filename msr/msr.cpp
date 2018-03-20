f31386acdfa9d129c0b00357821938fac64b6ac5
HeaJohnYeah <HeaJohn@163.com>
2018-03-20 22:01:01 +0800

    add msr and main

---
+++ msr/msr.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <set>
+#include <string>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <netdb.h>
+
+#include "../common/auto.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
